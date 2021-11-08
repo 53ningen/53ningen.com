f0ff3387d0947962cbc998ac3442af67439b690a
gomi_ningen <gomi2ngen@gmail.com>
2021-11-09 03:00:46 +0900

    prefetch の無効化

--- src/components/common/Link.tsx
+++ src/components/common/Link.tsx
@@ -20,22 +20,13 @@ export const NextLinkComposed = React.forwardRef<
   HTMLAnchorElement,
   NextLinkComposedProps
 >(function NextLinkComposed(props, ref) {
-  const {
-    to,
-    linkAs,
-    href,
-    replace,
-    scroll,
-    shallow,
-    prefetch,
-    locale,
-    ...other
-  } = props
+  const { to, linkAs, href, replace, scroll, shallow, locale, ...other } = props
 
   return (
     <NextLink
       href={to}
-      prefetch={prefetch}
+      // FIXME: https://stackoverflow.com/questions/68009879/nextjs-link-prefetch-returns-404
+      prefetch={false}
       as={linkAs}
       replace={replace}
       scroll={scroll}
