f484236cf21f280c2e7bd12a555a7aa43bf14a03
gomi_ningen <gomi2ngen@gmail.com>
2022-01-17 10:11:45 +0900

    amplify import storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,15 +1,38 @@
 export type AmplifyDependentResourcesAttributes = {
     "auth": {
-        "53ningen": {
+        "userPoolGroups": {
+            "AdministratorGroupRole": "string"
+        },
+        "53ningenAuth": {
             "IdentityPoolId": "string",
-            "IdentityPoolName": "string"
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
         }
     },
     "analytics": {
-        "53ningen": {
+        "53ningenAnalytics": {
             "Region": "string",
             "Id": "string",
             "appName": "string"
+        }
+    },
+    "function": {
+        "AdminQueries03897ed4": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "AdminQueries": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
         }
     }
 }
